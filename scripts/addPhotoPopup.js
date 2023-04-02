4a390be4e440e975248264c409c05d7d1200505f
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-04-02 17:18:17 +0900

    feat: Реализовать добавление фотокарточки

---
+++ scripts/addPhotoPopup.js
@@ -0,0 +1,48 @@
+const addPhotoPopup = document.querySelector('.popup_type_add-photo');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = addPhotoPopup.querySelector('.popup__close');
+const submitPhotoPopupButton = addPhotoPopup.querySelector('.popup__submit-button');
+const addPhotoPopupForm = addPhotoPopup.querySelector('.popup__form');
+const placeNameInput = addPhotoPopupForm.querySelector('.popup__input_type_place-name');
+const placeUrlInput = addPhotoPopupForm.querySelector('.popup__input_type_picture-url');
+const placeTitle = document.querySelector('.photo-card__text');
+const placeImage = document.querySelector('.photo-card__image')
+
+
+
+
+closePhotoPopup = () => {
+  addPhotoPopup.classList.remove('popup_open');
+};
+
+// disabledSubmitPhotoPopupButton = () => {
+//   if(placeNameInput.value.length === 0 || placeUrlInput.value.length === 0){
+//     submitPhotoPopupButton.setAttribute('disabled', true);
+//   }
+// }
+
+submitPhotoPopup = (event) => {
+  event.preventDefault();
+
+  const name = placeNameInput.value;
+  const link = placeUrlInput.value;
+  const photoData = {
+    name,
+    link,
+  };
+
+  renderPhotoElement(createPhotoCard(photoData));
+};
+
+
+
+addButton.addEventListener('click', () => {
+  openPopup(addPhotoPopup)
+})
+
+closeButton.addEventListener('click', ()=>{
+  closePopup(addPhotoPopup);
+})
+
+addPhotoPopupForm.addEventListener('submit', submitPhotoPopup);
+
--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -25,39 +25,3 @@ const initialCards = [
   }
 ];
 
-const photoCardTemplate = document.getElementById('photo-card-template')
-const photoGrid = document.querySelector('.elements__list')
-
-const createPhotoCard = (photoData) => {
-  const photoElement = photoCardTemplate.content.querySelector('.photo-card').cloneNode(true);
-  const photoName = photoElement.querySelector('.photo-card__text');
-  const photoImage= photoElement.querySelector('.photo-card__image');
-  const likeButton = photoElement.querySelector('.photo-card__like');
-  const deleteButton = photoElement.querySelector('.photo-card__delete');
-
-  const handleDelete = () => {
-    photoElement.remove();
-  };
-
-  const handleLike = () => {
-    likeButton.classList.toggle('photo-card__like_active')
-  };
-
-  deleteButton.addEventListener('click', (handleDelete));
-
-  likeButton.addEventListener('click', (handleLike));
-
-  photoName.textContent = photoData.name;
-  photoImage.src = photoData.link;
-  photoImage.alt = photoData.name;
-
-  return photoElement;
-}
-
-const renderPhotoElement = (photoElement) => {
-  photoGrid.prepend(photoElement);
-}
-
-initialCards.forEach((initialCards) => {
-  renderPhotoElement(createPhotoCard(initialCards));
-});
