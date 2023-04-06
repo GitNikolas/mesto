6102cb0de0921e12e592bf78b6b3ed2aa1cc3d85
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-04-06 15:21:43 +0900

    refactor: изменить метод работы с массивом полей ввода

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-const editLink=document.querySelector('.profile__edit-button');
-const profileEditPopup=document.querySelector('.popup_type_profile-edit');
-const profileEditPopupCloseButton=profileEditPopup.querySelector('.popup__close');
-const popupUserName=profileEditPopup.querySelector('.popup__input_type_user-name');
-const popupUserStatus=profileEditPopup.querySelector('.popup__input_type_user-status');
-const popupSaveButton=profileEditPopup.querySelector('.popup__submit-button');
-const profileEditPopupForm=profileEditPopup.querySelector('.popup__form');
-const userName=document.querySelector('.profile__name');
-const userStatus=document.querySelector('.profile__status');
+const editLink = document.querySelector('.profile__edit-button');
+const profileEditPopup = document.querySelector('.popup_type_profile-edit');
+const profileEditPopupCloseButton = profileEditPopup.querySelector('.popup__close');
+const popupUserName = profileEditPopup.querySelector('.popup__input_type_user-name');
+const popupUserStatus = profileEditPopup.querySelector('.popup__input_type_user-status');
+const popupSaveButton = profileEditPopup.querySelector('.popup__submit-button');
+const profileEditPopupForm = profileEditPopup.querySelector('.popup__form');
+const userName = document.querySelector('.profile__name');
+const userStatus = document.querySelector('.profile__status');
 const placeName = document.querySelector('.popup__input_type_place-name');
 const link = document.querySelector('.popup__input_type_picture-url')
 
@@ -18,20 +18,20 @@ const closePopup = (popup) => {
   popup.classList.remove('popup_open');
 };
 
-const disabledButton = (popupForm) => {
-  const input = popupForm.querySelectorAll('.popup__input');
-  const submitButton = popupForm.querySelector('.popup__submit-button');
+const togglePopupSubmitButtonState = (popupForm) => {
+  const inputs = popupForm.querySelectorAll('.popup__input');
+  const submitButtons = popupForm.querySelector('.popup__submit-button');
 
-  input.forEach((item) =>{
-    if(item.value.length === 0){
-      submitButton.setAttribute('disabled', true);
-    } else {
-      submitButton.removeAttribute('disabled');
-    }
-  })
+  const hasInvalidInputs = Array.from(inputs).some(item => item.value.length === 0);
+
+  if (hasInvalidInputs){
+    submitButtons.setAttribute('disabled', true);
+  } else {
+    submitButtons.removeAttribute('disabled');
+  }
 };
 
-function profileFormSubmit(event){
+function submitProfileForm(event){
   event.preventDefault();
   userName.textContent = popupUserName.value;
   userStatus.textContent = popupUserStatus.value;
@@ -42,32 +42,32 @@ editLink.addEventListener('click', () => {
   openPopup(profileEditPopup);
   popupUserName.value = userName.textContent ;
   popupUserStatus.value = userStatus.textContent;
-  disabledButton(profileEditPopup);
+  togglePopupSubmitButtonState(profileEditPopup);
 });
 
 profileEditPopup.addEventListener('input', () =>{
-  disabledButton(profileEditPopup);
+  togglePopupSubmitButtonState(profileEditPopup);
 })
 
 profileEditPopupCloseButton.addEventListener('click', () => {
   closePopup(profileEditPopup);
 });
 
-profileEditPopupForm.addEventListener('submit', profileFormSubmit);
+profileEditPopupForm.addEventListener('submit', submitProfileForm);
 
 // реализация создания новой карточки
 
 const photoCardTemplate = document.getElementById('photo-card-template');
 const photoGrid = document.querySelector('.elements__list');
+const photoViewPopup = document.querySelector('.popup_type_view-photo');
+const photoViewPopupCloseButton = photoViewPopup.querySelector('.popup__close');
 
 const createPhotoCard = (photoData) => {
   const photoElement = photoCardTemplate.content.querySelector('.photo-card').cloneNode(true);
-  const photoViewPopup = document.querySelector('.popup_type_view-photo');
   const photoName = photoElement.querySelector('.photo-card__text');
   const photoImage= photoElement.querySelector('.photo-card__image');
   const likeButton = photoElement.querySelector('.photo-card__like');
   const deleteButton = photoElement.querySelector('.photo-card__delete');
-  const photoViewPopupCloseButton = photoViewPopup.querySelector('.popup__close');
 
   const handleDelete = () => {
     photoElement.remove();
@@ -82,11 +82,7 @@ const createPhotoCard = (photoData) => {
     const openedPhoto=photoViewPopup.querySelector('.popup__image')
     const subtitlePhoto = photoViewPopup.querySelector('.popup__caption');
     openedPhoto.src=photoImage.src;
-    subtitlePhoto.innerHTML=photoImage.alt;
-  });
-
-  photoViewPopupCloseButton.addEventListener('click', () =>{
-    closePopup(photoViewPopup);
+    subtitlePhoto.textContent=photoImage.alt;
   });
 
   deleteButton.addEventListener('click', handleDelete);
@@ -100,6 +96,10 @@ const createPhotoCard = (photoData) => {
   return photoElement;
 }
 
+photoViewPopupCloseButton.addEventListener('click', () => {
+  closePopup(photoViewPopup);
+});
+
 const renderPhotoElement = (photoElement) => {
   photoGrid.prepend(photoElement);
 }
@@ -112,7 +112,7 @@ initialCards.forEach((initialCards) => {
 
 const addPhotoPopup = document.querySelector('.popup_type_add-photo');
 const addButton = document.querySelector('.profile__add-button');
-const closeButton = addPhotoPopup.querySelector('.popup__close');
+const closeaddPhotoPopupButton = addPhotoPopup.querySelector('.popup__close');
 const submitPhotoPopupButton = addPhotoPopup.querySelector('.popup__submit-button');
 const addPhotoPopupForm = addPhotoPopup.querySelector('.popup__form');
 const placeNameInput = addPhotoPopupForm.querySelector('.popup__input_type_place-name');
@@ -130,21 +130,22 @@ submitPhotoPopup = (event) => {
     link,
   };
 
+  addPhotoPopupForm.reset();
   renderPhotoElement(createPhotoCard(photoData));
   closePopup(addPhotoPopup);
 };
 
 addButton.addEventListener('click', () => {
-  disabledButton(addPhotoPopup);
+  togglePopupSubmitButtonState(addPhotoPopup);
   openPopup(addPhotoPopup);
 })
 
-closeButton.addEventListener('click', ()=>{
+closeaddPhotoPopupButton.addEventListener('click', ()=>{
   closePopup(addPhotoPopup);
 })
 
 addPhotoPopupForm.addEventListener('submit', submitPhotoPopup);
 
 addPhotoPopup.addEventListener('input', () => {
-  disabledButton(addPhotoPopup)
+  togglePopupSubmitButtonState(addPhotoPopup)
 });
